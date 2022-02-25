b433d3eb56346fa4bdc6c6db1075419dd49dd751
André <andreabenf@gmail.com>
2022-02-25 19:28:25 -0300

    Parametrizando e triangularizando 1 faixa
    Co-authored-by: Wictorgirardi <Wictor.girardi@gmail.com>

--- src/test.py
+++ src/test.py
@@ -13,3 +13,12 @@ def test_total_decucoes(dedu1, dedu2, dedu3, total):
     simuilacao.cadastrar_deducao("testeDeducao", dedu3)
     assert simuilacao.total_deducoes == total;
 
+@pytest.mark.parametrize("dedu1,dedu2,rend1,rend2", [(100, 200, 300, 600), (150, 220, 100, 470), (120, 340, 650, 1110)])
+def test_1_faixa(dedu1, dedu2, rend1, rend2):
+    simuilacao = simulador_IRPF.SimuladorIRPF()
+    simuilacao.cadastrar_deducao("testeDeducao", dedu1)
+    simuilacao.cadastrar_deducao("testeDeducao", dedu2)
+    simuilacao.cadastrar_rendimento("testeRendimento", rend1)
+    simuilacao.cadastrar_rendimento("testeRendimento", rend2)
+    assert simuilacao.calcular_primeira_faixa() == 0;
+
